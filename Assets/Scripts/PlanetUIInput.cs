a1e2ac403472170b6422812367762ddbb012c259
Misha <misha-sudar@yandex.ru>
2021-02-26 22:49:21 +0300

    Restructuring slider processing

---
+++ Assets/Scripts/PlanetUIInput.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Globalization;
+
+public class PlanetUIInput : MonoBehaviour
+{
+    [SerializeField] private Canvas canvas;
+    [Space]
+    [SerializeField] private Button generateButton;
+    [SerializeField] private Button clearButton;
+    [Space]
+    [SerializeField] private Toggle sphericalToggle;
+    [Space]
+    [SerializeField] private SliderIntInputTMP widthSlider;
+    [SerializeField] private SliderIntInputTMP heightSlider;
+    [SerializeField] private SliderIntInputTMP depthSlider;
+    [SerializeField] private SliderIntInputTMP minMassSlider;
+    [SerializeField] private SliderIntInputTMP maxMassSlider;
+    [SerializeField] private SliderIntInputTMP minStartSpeedSlider;
+    [SerializeField] private SliderIntInputTMP maxStartSpeedSlider;
+    [SerializeField] private TMP_InputField gInputField;
+    [Space]
+    [SerializeField] private TextMeshProUGUI minMassMesh;
+    [SerializeField] private TextMeshProUGUI maxMassMesh;
+    [SerializeField] private TextMeshProUGUI gMesh;
+
+    public event Action OnGenerateButtonClick;
+    public event Action OnClearButtonClick;
+    public event Action<bool> OnSphericalToggleClick;
+
+    public int Width { get { return widthSlider.value; } }
+    public int Height { get { return heightSlider.value; } }
+    public int Depth { get { return depthSlider.value; } }
+    public int MinMass { get { return minMassSlider.value; } }
+    public int MaxMass { get { return maxMassSlider.value; } }
+    public int MinStartSpeed { get { return minStartSpeedSlider.value; } }
+    public int MaxStartSpeed { get { return maxStartSpeedSlider.value; } }
+    public float G { get { return g; } }
+
+    private int width = 1;
+    private int height = 1;
+    private int depth = 1;
+    private int minMass = 5;
+    private int maxMass = 50;
+    private int minStartSpeed = 5;
+    private int maxStartSpeed = 10;
+    private float g = .1f;
+
+    private void Start()
+    {
+        SubscribeNonSliderInputs();
+        UpdateAllValues();
+        UpdateAllTexts();
+        generateButton.onClick.AddListener(() => { OnGenerateButtonClick.Invoke(); });
+        sphericalToggle.onValueChanged.AddListener((bool b) => { OnSphericalToggleClick.Invoke(b); });
+        clearButton.onClick.AddListener(() => { OnClearButtonClick.Invoke(); });
+    }
+
+    private void OnSliderUpdate(SliderIntInputTMP sliderIntInput , ref int value)
+    {
+        value = sliderIntInput.value;
+    }
+
+    private void SubscribeNonSliderInputs()
+    {
+        minMassSlider.OnValueChanged += () =>
+            {
+                maxMassSlider.slider.minValue = minMassSlider.value;
+                maxMassSlider.Refresh();
+            };
+        
+        gInputField.onEndEdit.AddListener(delegate { UpdateInputFieldText(gInputField, gMesh); });
+        gInputField.onEndEdit.AddListener(delegate { UpdateInputFieldValue(gInputField, ref g); });
+    }
+    
+    private void UpdateAllTexts()
+    {
+        UpdateInputFieldText(gInputField, gMesh);
+    }
+
+    private void UpdateAllValues()
+    {
+        widthSlider.SetValue(width);
+        heightSlider.SetValue(height);
+        depthSlider.SetValue(depth);
+        minMassSlider.SetValue(minMass);
+        maxMassSlider.SetValue(maxMass);
+        minStartSpeedSlider.SetValue(minStartSpeed);
+        maxStartSpeedSlider.SetValue(maxStartSpeed);
+        UpdateInputFieldValue(gInputField, ref g);
+    }
+
+    private void UpdateInputFieldValue(TMP_InputField inputField, ref float value)
+    {
+        value = float.Parse(inputField.text, CultureInfo.InvariantCulture.NumberFormat);
+    }
+
+    private void UpdateInputFieldText(TMP_InputField inputField, TextMeshProUGUI sliderText)
+    {
+        sliderText.text = (float.Parse(inputField.text, CultureInfo.InvariantCulture.NumberFormat)).ToString("F1", CultureInfo.InvariantCulture);
+    }
+}
---
+++ Assets/Scripts/Utility/SliderIntInputTMP.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Events;
+using System;
+
+[RequireComponent(typeof(Slider))]
+
+public class SliderIntInputTMP : MonoBehaviour, IInputUI
+{
+    [SerializeField] private TextMeshProUGUI sliderValueMesh;
+
+    public Slider slider;
+
+    public int value { get; private set; }
+
+    UnityAction<float> reaction;
+    public event Action OnValueChanged;
+    
+    void Start()
+    {
+        SubscribeToValueChange();
+    }
+
+    public void AddValueChangedListener(Action delegateToAdd)
+    {
+        OnValueChanged += delegateToAdd;
+    }
+
+    public void SubscribeToValueChange()
+    {
+        if (slider == null)
+        {
+            slider = GetComponent<Slider>();
+        }
+        reaction = delegate
+        {
+            UpdateValueFromSlider();
+            UpdateText();
+            OnValueChanged?.Invoke();
+        };
+            
+        slider.onValueChanged.AddListener(reaction);
+    }
+
+    public void UnsubscribeFromValueChange()
+    {
+        slider.onValueChanged.RemoveListener(reaction);
+    }
+
+    private void UpdateValueFromSlider()
+    {
+        this.value = Mathf.Clamp((int)slider.value, (int)slider.minValue, (int)slider.maxValue);
+    }
+    public void SetValue(int value)
+    {
+        this.value = Mathf.Clamp(value, (int)slider.minValue, (int)slider.maxValue);
+        UpdateText();
+    }
+
+    public void Refresh()
+    {
+        slider.value = Mathf.Clamp(slider.value, (int)slider.minValue, (int)slider.maxValue);
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        sliderValueMesh.text = value.ToString();
+    }
+}
